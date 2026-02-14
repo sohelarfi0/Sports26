cc4714ea034c9aed8c61a622d765fc1f8edbe1d5
sohel <sohelarfi0@gmail.com>
2026-02-14 23:44:15 +0530

    day-3

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -13,7 +13,7 @@ matchRouter.get('/',async(req,res)=>{
     const parsed=listMatchesQuerySchema.safeParse(req.query);
 
     if(!parsed.success){
-        return res.status(400).json({error:'Invalid query parameters.', details:JSON.stringify(parsed.error)});
+        return res.status(400).json({error:'Invalid query parameters.', details:parsed.error.issues});
     }
     const limit=Math.min(parsed.data.limit?? 50, MAX_LIMIT);
 
@@ -38,14 +38,15 @@ matchRouter.get('/',async(req,res)=>{
 matchRouter.post ('/',async (req,res)=>{
 
     const parsed=createMatchSchema.safeParse(req.body);
-    const {data:{startTime, endTime, homeScore,awayScore}}=parsed;
 
 
 
     if(!parsed.success){
-        return res.status(400).json({error:'Invalid payload.', details:JSON.stringify(parsed.error)});
+        return res.status(400).json({error:'Invalid payload.', details:parsed.error.issues});
 
     }
+        const {data:{startTime, endTime, homeScore,awayScore}}=parsed;
+
 
     try{
 
